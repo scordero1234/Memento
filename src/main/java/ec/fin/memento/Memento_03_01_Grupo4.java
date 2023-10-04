2ab4c54f54e598fcb4fce21c17b8cadbed07d3ef
s.corderoc <s.corderoc@jardinazuayo.fin.ec>
2023-10-04 08:42:11 -0500

    Initial commit

---
+++ src/main/java/ec/fin/memento/Memento_03_01_Grupo4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.fin.memento;
+
+/**
+ *
+ * @author s.corderoc
+ */
+public class Memento_03_01_Grupo4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
